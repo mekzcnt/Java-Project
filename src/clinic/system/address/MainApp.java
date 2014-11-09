17faf5672f20c499834daabc35e0285a727908b1
lask20 <s6070040@kmitl.ac.th>
2014-11-09 19:24:51 +0700

    add MedicineController

--- src/clinic/system/address/MainApp.java
+++ src/clinic/system/address/MainApp.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -20,6 +21,7 @@ public class MainApp extends Application {
 
         initRootLayout();
         showMainMenuOverview();
+        showMedicineOverview();
 	}
 
 	public void initRootLayout() {
@@ -54,6 +56,33 @@ public class MainApp extends Application {
             e.printStackTrace();
         }
     }
+	
+	public void showMedicineOverview() {
+		try {
+			// Load the fxml file and create a new stage for the popup dialog.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/MedicineOverview.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+
+			// Create the dialog Stage.
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Edit Person");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(primaryStage);
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
+
+			
+
+			// Show the dialog and wait until the user closes it
+			dialogStage.showAndWait();
+
+			//return controller.isOkClicked();
+		} catch (IOException e) {
+			e.printStackTrace();
+			//return false;
+		}
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
---
+++ src/clinic/system/address/view/MedicineOverviewController.java
@@ -0,0 +1,24 @@
+package clinic.system.address.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class MedicineOverviewController {
+
+    @FXML
+    private TableColumn<?, ?> qulity;
+
+    @FXML
+    private TextField search;
+
+    @FXML
+    private TableColumn<?, ?> code;
+
+    @FXML
+    private TableColumn<?, ?> price;
+
+    @FXML
+    private TableColumn<?, ?> name;
+
+}
