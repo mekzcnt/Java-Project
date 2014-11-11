f39a79c496378c993d372d7b2e3d4e6625e6c345
lask20 <s6070040@kmitl.ac.th>
2014-11-11 21:48:59 +0700

    Create Database

--- src/clinic/system/address/view/MedicineOverviewController.java
+++ src/clinic/system/address/view/MedicineOverviewController.java
@@ -10,9 +10,11 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import java.sql.*;
 
 public class MedicineOverviewController {
 
+    
 	private MainApp mainApp;
     @FXML
     private TableColumn<?, ?> qulity;
@@ -47,6 +49,53 @@ public class MedicineOverviewController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    	
+    }
+    public void handleEdit() {
+    	
+		try {
+			
+			Stage dialogStage = new Stage();
+	    	FXMLLoader loader = new FXMLLoader(getClass().getResource("MedicineAddWindow.fxml"));
+	    	AnchorPane root;
+			root = (AnchorPane)loader.load();
+			//DialogController controller = (DialogController) loader.getController();
+	    	Scene scene = new Scene(root);
+	    	dialogStage.setScene(scene);
+	    	dialogStage.showAndWait();
+	    	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+    public void handleDelete() {
+    	
+    	
+    }
+    public void handleSearch() {
+    	Connection c = null;
+        Statement stmt = null;
+        try {
+          Class.forName("org.sqlite.JDBC");
+          c = DriverManager.getConnection("jdbc:sqlite:CMSDatabase.db");
+
+          stmt = c.createStatement();
+          String sql = "CREATE TABLE Medicine " +
+                       "(ID INT PRIMARY KEY     NOT NULL," +
+                       " NAME           TEXT    NOT NULL, " + 
+                       " Amout          INT     NOT NULL, " + 
+                       " Price          INT, " + 
+                       " Details        CHAR(50))"; 
+          stmt.executeUpdate(sql);
+          stmt.close();
+          c.close();
+        } catch ( Exception e ) {
+          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+          System.exit(0);
+        }
+        System.out.println("Table created successfully");
     	
     }
     public void setMainApp(MainApp mainApp) {
