35d23fe7f229840c95179df985c0193fdb1f4088
lask20 <s6070040@kmitl.ac.th>
2014-11-11 21:35:10 +0700

    ใส่ add ใน  medicine

--- src/clinic/system/address/view/MedicineOverviewController.java
+++ src/clinic/system/address/view/MedicineOverviewController.java
@@ -1,9 +1,15 @@
 package clinic.system.address.view;
 
+import java.io.IOException;
+
 import clinic.system.address.MainApp;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class MedicineOverviewController {
 
@@ -23,6 +29,26 @@ public class MedicineOverviewController {
     @FXML
     private TableColumn<?, ?> name;
 
+    @FXML
+    public void handleADD() {
+    	
+		try {
+			
+			Stage dialogStage = new Stage();
+	    	FXMLLoader loader = new FXMLLoader(getClass().getResource("MedicineAddWindow.fxml"));
+	    	AnchorPane root;
+			root = (AnchorPane)loader.load();
+			//DialogController controller = (DialogController) loader.getController();
+	    	Scene scene = new Scene(root);
+	    	dialogStage.setScene(scene);
+	    	dialogStage.showAndWait();
+	    	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
     public void setMainApp(MainApp mainApp) {
 		// TODO Auto-generated method stub
 		this.mainApp = mainApp;
