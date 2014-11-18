2d1acbb7d08be26677381e624072dcbefea501f7
lask20 <s6070040@kmitl.ac.th>
2014-11-18 18:24:42 +0700

    add profile

---
+++ src/clinic/system/address/model/profile.java
@@ -0,0 +1,12 @@
+package clinic.system.address.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+public class profile {
+	private StringProperty FName;
+	private StringProperty LName;
+}
