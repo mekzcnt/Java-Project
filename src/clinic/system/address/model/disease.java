e1e090e15ba903759c07ecf90859cf334640fcfc
lask20 <s6070040@kmitl.ac.th>
2014-11-20 01:17:39 +0700

    aa

--- src/clinic/system/address/model/disease.java
+++ src/clinic/system/address/model/disease.java
@@ -6,11 +6,11 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class disease {
-	private final StringProperty FName;
-	private final StringProperty LName;
-	private final IntegerProperty ID;
-	private final StringProperty disease;
-	private final StringProperty description;
+	private StringProperty FName;
+	private StringProperty LName;
+	private IntegerProperty ID;
+	private StringProperty disease;
+	private StringProperty description;
 	
 	public disease(String FName, String LName, int ID, String disease, String description) {
 		this.FName = new SimpleStringProperty(FName);
@@ -18,6 +18,10 @@ public class disease {
 		this.ID = new SimpleIntegerProperty(ID);
 		this.disease = new SimpleStringProperty(disease);
 		this.description = new SimpleStringProperty(description);
+	}
+	public void name(String FName, String LName) {
+		this.FName = new SimpleStringProperty(FName);
+		this.LName = new SimpleStringProperty(LName);
 	}
 	public StringProperty getFName() {
 		return FName;
