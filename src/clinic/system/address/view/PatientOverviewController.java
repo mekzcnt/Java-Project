4bfbb380e3641f885e8b24b64bd16b8a6029b12a
lask20 <s6070040@kmitl.ac.th>
2014-11-20 00:38:58 +0700

    หน้าอัศจรร add ยา ได้ แล้ว อิอิ

--- src/clinic/system/address/view/PatientOverviewController.java
+++ src/clinic/system/address/view/PatientOverviewController.java
@@ -1,11 +1,24 @@
 package clinic.system.address.view;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import clinic.system.address.MainApp;
 import clinic.system.address.model.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -30,7 +43,11 @@ public class PatientOverviewController {
     @FXML
     private Button AddMoneyButton;
 
-
+    @FXML
+    private ListView<medicine> MedicineList;
+    private ObservableList<medicine> listViewData = FXCollections.observableArrayList();
+    private ObservableList<medicine> data = FXCollections.observableArrayList();
+    
 	@FXML
     private Label Weight;
 
@@ -66,6 +83,9 @@ public class PatientOverviewController {
 
     @FXML
     private Label BirthDate;
+    
+    @FXML
+    private TableView<medicine> MedicineTable;
 
     public void setDialogStage(Stage dialogStage) {
         this.dialogStage = dialogStage;
@@ -80,27 +100,95 @@ public class PatientOverviewController {
 
 	}
     
-    private void initialize() {
-    	Name1.setText(( this.profile).getFName().getValue());
-    	Surname1.setText(( this.profile).getLName().getValue());
-    	ID1.setText(""+( this.profile).getID().getValue());
-    	Height.setText(""+( this.profile).getHeight().getValue());
-    	Age.setText(""+( this.profile).getOld().getValue());
-    	BirthDate.setText(( this.profile).getBirthday().getValue());
-    	Weight.setText(""+( this.profile).getWeight().getValue());
-    	BloodPressure.setText(""+( this.profile).getPressure().getValue());
-    	
-    }
+
     public void setProfile(profile profile) {
 		this.profile = profile;
 		Name1.setText(profile.getFName().getValue());
-		Surname1.setText(( this.profile).getLName().getValue());
-    	ID1.setText(""+( this.profile).getID().getValue());
-    	Height.setText(""+( this.profile).getHeight().getValue());
-    	Age.setText(""+( this.profile).getOld().getValue());
-    	BirthDate.setText(( this.profile).getBirthday().getValue());
-    	Weight.setText(""+( this.profile).getWeight().getValue());
-    	BloodPressure.setText(""+( this.profile).getPressure().getValue());
-	}
+		Surname1.setText(profile.getLName().getValue());
+    	ID1.setText(""+this.profile.getID().getValue());
+    	Height.setText(""+this.profile.getHeight().getValue());
+    	Age.setText(""+profile.getOld().getValue());
+    	BirthDate.setText(profile.getBirthday().getValue());
+    	Weight.setText(""+profile.getWeight().getValue());
+    	BloodPressure.setText(""+profile.getPressure().getValue());
+    	
+    	MedicineList.setItems(listViewData);
+    	MedicineList.setCellFactory((list) -> {
+    	    return new ListCell<medicine>() {
+    	        @Override
+    	        protected void updateItem(medicine item, boolean empty) {
+    	            super.updateItem(item, empty);
 
+    	            if (item == null || empty) {
+    	                setText(null);
+    	            } else {
+    	                setText(item.getMName().getValue() + " " + item.getPrice().getValue());
+    	            }
+    	        }
+    	    };
+    	});
+    	
+    	MedicineName.setCellValueFactory(cellData -> cellData.getValue().getMName());
+    	MedicinePrice.setCellValueFactory(cellData -> cellData.getValue().getPrice());
+    	
+	}
+    @FXML
+    public void handleSearch() {
+    	if (MedicineSearchBox.getText().isEmpty()) {
+    		MedicineList.setVisible(false);
+    	}
+    	else {
+    		MedicineList.setVisible(true);
+    		find(MedicineSearchBox.getText());
+    	}
+    	
+    }
+    public void find(String word) {
+    	Connection c = null;
+        Statement stmt = null;
+        try {
+        	Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:CMSDatabase.db");
+            c.setAutoCommit(false);
+
+          stmt = c.createStatement();
+          
+          String sql = "SELECT * FROM Medicine " +
+                  "WHERE NAME LIKE '%"+word+"%' OR ID LIKE '%"+word+"%';" ;
+          
+          ResultSet rs = stmt.executeQuery(sql);
+          listViewData.clear();
+          while ( rs.next() ) {
+            
+        	  listViewData.add(new medicine(rs.getInt("ID"),rs.getString("NAME"),rs.getInt("Price"),rs.getInt("Amout"),rs.getString("Details")));
+
+          }
+          rs.close();
+          stmt.close();
+          c.close();
+        } catch ( Exception e ) {
+          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+          System.exit(0);
+        }
+        
+    }
+    public void handleSubmit() {
+    	System.out.print("ss");
+    	//System.out.print(MedicineList.getSelectionModel().getSelectedItem().getMName().getValue());
+    	data.add(MedicineList.getSelectionModel().getSelectedItem());
+    	MedicineSearchBox.setText("");
+    	MedicineTable.getItems().clear();
+    	MedicineTable.getItems().setAll(data);
+    	handleSearch();
+    	displayprice();
+
+    }
+    
+    private void displayprice() {
+    	int price = 0;
+    	for (medicine each:data) {
+    		price += each.getPrice().intValue();
+    	}
+    	SumMedicinePrice.setText(""+price);
+    }
 }
