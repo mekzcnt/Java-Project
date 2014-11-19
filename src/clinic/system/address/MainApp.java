8603171162d612fb740d48f11e87102029f07b76
Mekz <mekzcnt@gmail.com>
2014-11-19 19:52:58 +0700

    จัดย่อหน้า-บรรทัด นิดหน่อย

--- src/clinic/system/address/MainApp.java
+++ src/clinic/system/address/MainApp.java
@@ -161,9 +161,7 @@ public class MainApp extends Application {
 			// Show the dialog and wait until the user closes it
 			dialogStage.showAndWait();
 			
-			
 			//Profilecontroller.setMainApp(this);
-
 			//return controller.isOkClicked();
 		} catch (IOException e) {
 			e.printStackTrace();
